f3bba4615881e37291682d8ba4dc92d9d140da4b
Surabhi <surabhi.samaddar5@gmail.com>
2018-10-09 20:30:12 +0530

    Resolution in camera

---
+++ resolution.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 11:04:33 2018
+
+@author: SURABHI
+"""
+
+import cv2
+print(cv2.__version__)
+cap = cv2.VideoCapture(0)    
+def make_1080p():
+   cap.set(3, 1920)   
+   cap.set(4, 1080)
+
+def make_720p():
+    cap.set(3, 1280)
+    cap.set(4, 720)
+
+def make_480p():
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+def change_res(width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+make_480p()
+#change_res(1280, 720)
+
+while True:
+    ret,frame=cap.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()    
